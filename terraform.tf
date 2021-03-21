d9f822128b5be6ed28f667ede379a56f84f3fab6
prasanths2892 <80938221+prasanths2892@users.noreply.github.com>
2021-03-21 22:38:53 +0530

    Delete terraform.tf

--- terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "data-skp-rg1"
-    storage_account_name = "pyddatablob"
-    container_name       = "terraform-backend"
-    key                  = "dev_terraform.tfstate"
-  }
-}
